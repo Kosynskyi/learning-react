099ba22a2ea45e806331bf53b42ad9b0dd225a67
Kosynskyi <kosynskyi.vova@gmail.com>
2022-11-19 17:26:02 +0200

    save code

--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 
 import { Event } from 'components/Event';
-import css from './EventBoard.module.css';
+import { Board } from './EventBoard.styled';
 
 export const EventBoard = ({ events }) => {
   return (
-    <div className={css.evenBoard}>
+    <Board>
       {events.map(({ name, location, speaker, type, time: { start, end } }) => (
         <Event
           key={name}
@@ -17,7 +17,7 @@ export const EventBoard = ({ events }) => {
           end={end}
         />
       ))}
-    </div>
+    </Board>
   );
 };
 
